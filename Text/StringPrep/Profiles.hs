4445e4b931783bdd08e79c1ad0e1d986428eaa70
Philipp Balzarek <p.balzarek@googlemail.com>
2013-12-09 23:53:00 +0100

    unify SASLPrep profiles

--- Text/StringPrep/Profiles.hs
+++ Text/StringPrep/Profiles.hs
@@ -3,14 +3,24 @@
 -- | Profiles as defined by various sources
 module Text.StringPrep.Profiles
   ( namePrepProfile
-  , saslPrepQuery
-  , saslPrepStore
+  , saslPrepProfile
   ) where
 
 
 import qualified Data.Set as Set
 import           Data.Text (Text, singleton)
 import           Text.StringPrep
+
+-- | Nameprep profile (RFC 3491)
+namePrepProfile :: Bool -> StringPrepProfile
+namePrepProfile allowUnassigned =
+	Profile {
+		maps = [b1,b2],
+		shouldNormalize = True,
+		prohibited = (if allowUnassigned then id else (a1:))
+                               [c12,c22,c3,c4,c5,c6,c7,c8,c9],
+		shouldCheckBidi = True
+	}
 
 nonAsciiSpaces :: Set.Set Char
 nonAsciiSpaces = Set.fromList [ '\x00A0', '\x1680', '\x2000', '\x2001', '\x2002'
@@ -20,28 +30,16 @@ nonAsciiSpaces = Set.fromList [ '\x00A0', '\x1680', '\x2000', '\x2001', '\x2002'
                               ]
 
 
-namePrepProfile :: Bool -> StringPrepProfile
-namePrepProfile allowUnassigned =
-	Profile {
-		maps = [b1,b2],
-		shouldNormalize = True,
-		prohibited = (if allowUnassigned then [] else [a1]) ++ [c12,c22,c3,c4,c5,c6,c7,c8,c9],
-		shouldCheckBidi = True
-	}
-
 toSpace :: Char -> Text
 toSpace x = if x `Set.member` nonAsciiSpaces then " " else singleton x
 
-saslPrepQuery :: StringPrepProfile
-saslPrepQuery = Profile
-    [b1, toSpace]
-    True
-    [c12, c21, c22, c3, c4, c5, c6, c7, c8, c9]
-    True
-
-saslPrepStore :: StringPrepProfile
-saslPrepStore = Profile
-    [b1, toSpace]
-    True
-    [a1, c12, c21, c22, c3, c4, c5, c6, c7, c8, c9]
-    True
+-- | SASLPrep profile (RFC 4013). The parameter determines whether unassigned
+-- charater are allowed (query behaviour) or disallowed (store)
+saslPrepProfile :: Bool -> StringPrepProfile
+saslPrepProfile allowUnassigned = Profile
+    { maps = [b1, toSpace]
+    , shouldNormalize = True
+    , prohibited = (if allowUnassigned then id else (a1:))
+                      [c12, c21, c22, c3, c4, c5, c6, c7, c8, c9]
+    , shouldCheckBidi = True
+    }
